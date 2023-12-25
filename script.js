750ca285855a57795f04cf4c384f4cba361b1840
Ujek24 <117275500+Ujek24@users.noreply.github.com>
2023-12-25 13:07:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+// Ambil data dari spreadsheet
 function getDataFromSpreadsheet() {
-    var spreadsheetURL = "https://docs.google.com/spreadsheets/d/19NkLclKSTXMFxQ4ha2sUlnZze47Z-t5IwFY_ZrdLwew/edit#gid=1838407717";
+    // Ganti URL spreadsheet dengan URL spreadsheet Anda
+    var spreadsheetURL = "https://docs.google.com/spreadsheets/d/19NkLclKSTXMFxQ4ha2sUlnZze47Z-t5IwFY_ZrdLwew/edit?usp=sharing";
 
+    // Gunakan Google Sheets API untuk mengambil data JSON
     $.getJSON(spreadsheetURL, function (data) {
+        // Proses data spreadsheet di sini
         displayData(data);
     });
 }
 
+// Tampilkan data di halaman HTML
 function displayData(data) {
     var spreadsheetDataElement = document.getElementById("spreadsheet-data");
 
-
+    // Tampilkan data sesuai kebutuhan
+    // Contoh: Menampilkan nama kolom pertama dari setiap baris
     data.feed.entry.forEach(function (entry) {
         var cellData = entry.content.$t;
         spreadsheetDataElement.innerHTML += "<p>" + cellData + "</p>";
     });
 }
 
+// Panggil fungsi untuk mengambil data dari spreadsheet saat halaman dimuat
 $(document).ready(function () {
     getDataFromSpreadsheet();
 });
