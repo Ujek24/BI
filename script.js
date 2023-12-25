11c75a149eb4a17643e158b6fbd2a6690273fdb1
Ujek24 <117275500+Ujek24@users.noreply.github.com>
2023-12-25 13:01:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// File: script.js
+function getDataFromSpreadsheet() {
+    var spreadsheetURL = "https://docs.google.com/spreadsheets/d/19NkLclKSTXMFxQ4ha2sUlnZze47Z-t5IwFY_ZrdLwew/edit#gid=1838407717";
 
-// Contoh fungsi JavaScript
-function greet(name) {
-  return "Hello, " + name + "!";
+    $.getJSON(spreadsheetURL, function (data) {
+        displayData(data);
+    });
 }
 
-// Contoh penggunaan fungsi
-var person = "John";
-var greeting = greet(person);
-console.log(greeting);
+function displayData(data) {
+    var spreadsheetDataElement = document.getElementById("spreadsheet-data");
+
+
+    data.feed.entry.forEach(function (entry) {
+        var cellData = entry.content.$t;
+        spreadsheetDataElement.innerHTML += "<p>" + cellData + "</p>";
+    });
+}
+
+$(document).ready(function () {
+    getDataFromSpreadsheet();
+});
