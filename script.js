0559c32b1eb5bbaabe52dd00310c3b69ca4598bb
Ujek24 <117275500+Ujek24@users.noreply.github.com>
2023-12-25 12:55:25 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// File: script.js
+
+// Contoh fungsi JavaScript
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+// Contoh penggunaan fungsi
+var person = "John";
+var greeting = greet(person);
+console.log(greeting);
